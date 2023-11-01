2e950c632bb6ba45985ae1a4796a4fb4b29da755
Agustin Migliorero <agustinmigliorero@hotmail.com>
2023-10-31 21:39:39 -0300

    Final clase 31-10-23

--- Cursos/admin.py
+++ Cursos/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Curso, Alumno, Instructor
+
+admin.site.register(Curso)
+admin.site.register(Alumno)
+admin.site.register(Instructor)
 
 # Register your models here.
--- Cursos/models.py
+++ Cursos/models.py
@@ -2,24 +2,48 @@ from django.db import models
 
 
 class Curso(models.Model):
-    nombre = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=100)
+    horario = models.CharField(max_length=100)
+    diasDeLaSemana = models.CharField(max_length=100)
+    fechaDeInicio = models.DateField()
+    fechaDeFinalizacion = models.DateField()
+    instructor = models.ForeignKey(
+        "Instructor", on_delete=models.CASCADE, blank=True, null=True
+    )
+    alumnos = models.ManyToManyField("Alumno", blank=True, null=True)
 
     def __str__(self):
         return self.nombre
 
 
 class Alumno(models.Model):
-    nombre = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField()
+    fechaNacimiento = models.DateField()
+    documento = models.IntegerField()
+    domicilio = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=100)
+    genero = models.CharField(max_length=100)
+    # cursos = models.ManyToManyField("Curso")
 
     def __str__(self):
-        return self.nombre
+        return f"{self.nombre}, {self.apellido}"
 
 
 class Instructor(models.Model):
-    nombre = models.CharField(max_length=50)
+    nombre = models.CharField(max_length=100)
+    apellido = models.CharField(max_length=100)
+    email = models.EmailField()
+    fechaNacimiento = models.DateField()
+    documento = models.IntegerField()
+    domicilio = models.CharField(max_length=100)
+    telefono = models.CharField(max_length=100)
+    genero = models.CharField(max_length=100)
+    # cursos = models.ManyToManyField("Curso")
 
     def __str__(self):
-        return self.nombre
+        return f"{self.nombre}, {self.apellido}"
 
 
 # Create your models here.
