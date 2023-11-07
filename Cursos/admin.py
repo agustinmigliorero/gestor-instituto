9e9e3fa5c19e8ee69d5ca7a8126bc6fb4a06378b
Agustin Migliorero <agustinmigliorero@hotmail.com>
2023-11-07 19:52:18 -0300

    Commit 7-11

--- Cursos/admin.py
+++ Cursos/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from .models import Curso, Alumno, Instructor
 
-admin.site.register(Curso)
+
+class filtrarAlumnos(admin.ModelAdmin):
+    filter_horizontal = ("alumnos",)
+
+
+admin.site.register(Curso, filtrarAlumnos)
 admin.site.register(Alumno)
 admin.site.register(Instructor)
 
--- Cursos/views.py
+++ Cursos/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Curso
+
+
+def index(request):
+    cursos = Curso.objects.all()
+    context = {
+        "cursos": cursos,
+    }
+    return render(request, "index.html", context)
+
+
+def verCurso(request, id_curso):
+    curso = Curso.objects.get(id=id_curso)
+    context = {"curso": curso}
+    return render(request, "ver-curso.html", context)
+
 
 # Create your views here.
